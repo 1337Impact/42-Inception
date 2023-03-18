24d8bae5fa3de05ebc068faec9dbc57dc493569d
Mohammed Benkhattab <mbenkhat@student.1337.ma>
2023-03-18 16:16:05 -0400

    just right

---
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+service mysql start
+
+sleep 2
+
+sed -i 's/bind-address            = 127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's/#port/port/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+
+mysql -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' WITH GRANT OPTION;"
+
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+service mysql stop
---
+++ srcs/requirements/wordpress/tools/install_wp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#installing wp-cli
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+#download wordpress
+cd /var/www/html/
+wp core download --allow-root
+chown -R www-data:www-data /var/www/html/
+chmod -R 755 /var/www/html/
+
+
+sed -i '36 s/\/run\/php\/php7.3-fpm.sock/9000/' /etc/php/7.3/fpm/pool.d/www.conf
+
+#configure wordpress
+cp ./wp-config-sample.php ./wp-config.php
+
+sed -i "s|database_name_here|${DB_NAME}|g" ./wp-config.php
+sed -i "s|username_here|${DB_USER}|g" ./wp-config.php
+sed -i "s|password_here|${DB_PASS}|g" ./wp-config.php
+sed -i "s|localhost|${DB_HOST}|g" ./wp-config.php
+
+# exec "$@"
